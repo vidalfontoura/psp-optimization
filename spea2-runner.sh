bd980901fa066abf0e580823a94744ea34d2e359
Vidal Daniel da Fontoura <vdfontoura@inf.ufpr.br>
2015-12-08 10:47:01 -0200

    Adding sh to run code

---
+++ spea2-runner.sh
@@ -0,0 +1,12 @@
+nohup spea2-daemon
+	200,250,300,350,400,1000 \
+	40000,60000,70000,100000 \
+	SinglePointCrossover,IntegerTwoPointsCrossover \
+	BitFlipMutation \
+	200,300 \
+	0.9 \
+	0.01 \
+	30 \
+	20 \
+	results-chapter\ 
+	PPHHHPHHHHHHHHPPPHHHHHHHHHHPHPPPHHHHHHHHHHHHPPPPHHHHHHPHHPHP &
